59b040120df012157916f439ad6faeeccfefc874
kirushara <98322436+kirushara@users.noreply.github.com>
2023-09-17 11:24:07 +0300

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello DEV!");
+        System.out.println("1");
+        System.out.println("2");
     }
 }
